6dff776fe318146d716fa51b28e34adc26cdf1fa
Ali Şimşek <alisimsekodemis@gmail.com>
2026-05-03 17:53:56 +0300

    en son sürüm6

--- src/App.jsx
+++ src/App.jsx
@@ -94,8 +94,7 @@ const DEPO_LOCATIONS = [
 // --- GEMINI API CALLER ---
 const callGeminiAPI = async (prompt, isJson = false) => {
   const apiKey = "AIzaSyBdjE5hi3q60eODvLNh7GEOaa9_i7r8BOw"; // Sistem runtime'da otomatik tanımlar
-  const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-preview-09-2025:generateContent?key=${apiKey}`;
-
+  const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${apiKey}`;
   const payload = {
     contents: [{ parts: [{ text: prompt }] }],
   };
